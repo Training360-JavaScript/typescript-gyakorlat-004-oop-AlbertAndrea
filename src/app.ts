8ee1f1353f27ac2991f05c7f941fd743c79e3c7b
AlbertAndrea <92324558+AlbertAndrea@users.noreply.github.com>
2022-01-07 19:11:46 +0100

    solved

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 // Importáld be a HumanHero és TransformerHero osztályokat.
-
+import { HumanHero } from './hero';
+import { TransformerHero } from './transformer';
 
 /**
  * Állítsd be helyesen a változót!
@@ -8,7 +9,11 @@
  * Hozz létre benne 3 HumanHero példányt, szabadon választott adatokkal.
  * @var {HumanHero[]} humans
  */
-export const humans: any[] = [];
+export const humans: HumanHero[] = [
+    new HumanHero(1, "Andi", "female", 33, 5),
+    new HumanHero(2, "Bence", "male", 22, 4),
+    new HumanHero(3, "Nara", "female", 7, 6),
+];
 
 /**
  * Állítsd be helyesen a változót!
@@ -17,4 +22,8 @@ export const humans: any[] = [];
  * Hozz létre benne 3 TransformerHero példányt, szabadon választott adatokkal.
  * @var {TransformerHero[]} transformers
  */
-export const transformers: any[] = [];
+export const transformers: TransformerHero[] = [
+    new TransformerHero(1, "Andi2", 10 , 11, "Albert"),
+    new TransformerHero(2, "Bence2", 10 , 11, "Verdes"),
+    new TransformerHero(3, "Nara2", 10 , 11, "Albert"),
+];
